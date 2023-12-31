00913c0bba46768d451a6a1ae39e7b71d4538225
Emzzy241 <emzzyoluwole@gmail.com>
2023-12-31 21:44:57 +0100

    add controllers

---
+++ Parcels/Controllers/Parcel/ParcelController.cs
@@ -0,0 +1,41 @@
+using Microsoft.AspNetCore.Mvc;
+using Parcels.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Parcels.Controllers
+{
+    public class ParcelController : Controller
+    {
+        [HttpGet("/parcels")]
+        public ActionResult Index()
+        {
+            List<Parcel> myParcelsList = Parcel.GetAllParcels();
+            return View(myParcelsList);
+        }
+
+        [HttpGet("/parcels/new")]
+        public ActionResult New()
+        {
+            return View();
+        }
+
+        // View to display the parcel
+        [HttpPost("/parcels")]
+        public ActionResult Create(string newParcelName, string newParcelDimensions, int newParcelsWeight)
+        {
+            Parcel newParcel = new Parcel(newParcelName, newParcelDimensions, newParcelsWeight);
+
+            return RedirectToAction("Index");
+        }
+       
+        [HttpGet("/parcels/{id}")]
+        public ActionResult Show(int id)
+        {
+           Parcel foundParcel = Parcel.FindParcel(id);
+
+            return View(foundParcel);
+        }
+
+    }
+}
